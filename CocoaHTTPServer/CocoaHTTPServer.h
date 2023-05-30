e7f3fcb1149041fa58f91ee81c0318c6ddce310a
boluomeng <liyunlong@outlook.com>
2023-05-30 18:12:20 +0800

    first commit

---
+++ CocoaHTTPServer/CocoaHTTPServer.h
@@ -0,0 +1,18 @@
+//
+//  CocoaHTTPServer.h
+//  CocoaHTTPServer
+//
+//  Created by 李云龙 on 2023/5/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaHTTPServer.
+FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
+
+//! Project version string for CocoaHTTPServer.
+FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaHTTPServer/PublicHeader.h>
+
+
